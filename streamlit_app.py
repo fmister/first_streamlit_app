cddc1a6c162898e0174f062e0234e1b8c877a7e4
fmister <126615059+fmister@users.noreply.github.com>
2023-03-07 16:41:56 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,19 +22,19 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 st.dataframe(fruits_to_show)
 
-st.header('Fruityvice Fruit Advice!')
-fruit_choice = st.text_input('What fruit would you like information about?','Kiwi')
-st.write('The user entered ', fruit_choice)
-
 #New Section to display fruitvice api response
-fruityvice_response = rq.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-#st.text(fruityvice_response.json()) #just writes the data to the screen
-
-#take the json version of the response and normalize it
-fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
-#output it the screen as a table
-st.dataframe(fruityvice_normalized)
-
+st.header('Fruityvice Fruit Advice!')
+try:
+  fruit_choice = st.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+      stremlit.error("Please select a fruit to get information.")
+  else:
+    fruityvice_response = rq.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+    st.dataframe(fruityvice_normalized)
+except URLError as e:
+    st.error()
+    
 # don't run anything past here while we troubleshoot
 st.stop()
 
